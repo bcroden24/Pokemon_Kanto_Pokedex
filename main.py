eff8184792bcdc8dee1e48e8f76cefb41979201f
bcroden24 <ben.roden@outlook.com>
2026-05-06 22:02:17 +0800

    created type weakness dictionary and functions to output weakness and strengths of a given type

--- main.py
+++ main.py
@@ -6,14 +6,13 @@ from sqlalchemy import create_engine
 #import the pokemon dict from scraper program
 pokemon_data = pokemon
 
-# for pokemon in pokemon_data:
-#     print(pokemon)
-
+# get the headers
 headers = pokemon_data[0].keys()
 
-
+# create dataframe to store pokemon data
 df = pd.DataFrame(pokemon_data)
 
+# type conversions
 df['HP'] = df['HP'].astype('Int32')
 df['Att'] = df['Att'].astype('Int32')
 df['Def'] = df['Def'].astype('Int32')
@@ -21,13 +20,32 @@ df['S.Att'] = df['S.Att'].astype('Int32')
 df['S.Def'] = df['S.Def'].astype('Int32')
 df['Spd'] = df['Spd'].astype('Int32')
 
-# print(df)
-
+# create sql engine
 engine = create_engine(DATABASE_URL)
 
+# connect to db and create kanto pokedex table
 df.to_sql('kanto_pokedex', con=engine, if_exists='replace', index=False)
 
 
 
 
 
+weakness_matrix = {
+    'Bug': ['Fire', 'Flying', 'Rock'],
+    'Dark': ['Bug', 'Fighting'],
+    'Dragon': ['Dragon', 'Ice'],
+    'Electric': ['Ground'],
+    'Fighting': ['Flying', 'Psychic'],
+    'Fire': ['Ground', 'Rock', 'Water'],
+    'Flying': ['Electric', 'Ice', 'Rock'],
+    'Ghost': ['Dark', 'Ghost'],
+    'Grass': ['Bug', 'Fire', 'Flying', 'Ice', 'Poison'],
+    'Ground': ['Grass', 'Ice', 'Water'],
+    'Ice': ['Fighting', 'Fire', 'Rock', 'Steel'],
+    'Normal': ['Fighting'],
+    'Poison': ['Ground', 'Psychic'],
+    'Psychic': ['Bug', 'Dark', 'Ghost'],
+    'Rock': ['Fighting', 'Grass', 'Ground', 'Steel', 'Water'],
+    'Steel': ['Fighting', 'Fire', 'Ground'],
+    'Water': ['Electric', 'Grass']
+    }
--- types_matrix.py
+++ types_matrix.py
@@ -1,19 +1,41 @@
-type_matrix = {
+weakness_matrix = {
+    'Bug': ['Fire', 'Flying', 'Rock'],
+    'Dark': ['Bug', 'Fighting'],
+    'Dragon': ['Dragon', 'Ice'],
+    'Electric': ['Ground'],
+    'Fighting': ['Flying', 'Psychic'],
+    'Fire': ['Ground', 'Rock', 'Water'],
+    'Flying': ['Electric', 'Ice', 'Rock'],
+    'Ghost': ['Dark', 'Ghost'],
+    'Grass': ['Bug', 'Fire', 'Flying', 'Ice', 'Poison'],
+    'Ground': ['Grass', 'Ice', 'Water'],
+    'Ice': ['Fighting', 'Fire', 'Rock', 'Steel'],
     'Normal': ['Fighting'],
-    'Fire': ['Water', 'Ground', 'Rock'],
-    'Water': ['Grass', 'Electric'],
-    'Grass': ['Fire', 'Ice', 'Poison', 'Flying', 'Bug']
-}
-# more to come
+    'Poison': ['Ground', 'Psychic'],
+    'Psychic': ['Bug', 'Dark', 'Ghost'],
+    'Rock': ['Fighting', 'Grass', 'Ground', 'Steel', 'Water'],
+    'Steel': ['Fighting', 'Fire', 'Ground'],
+    'Water': ['Electric', 'Grass']
+    }
 
 # ----- ADD METHOD TO GET TYPES FROM POKEMON
 
-my_types = ['Fire', 'Water']
+# my_types = ['Fire', 'Water']
 
-def check_weakness(t):
-    for i in t:
-        print(type_matrix[i])
-    # return type_matrix[t]
+# check weakness types using type dict, return values of the whichever key matches input value t
+def check_weakness(t, weakness):
+    return weakness[t]
+        
+# Use weakness dict to find strong against types, return keys which values include input value t
+def check_strength(t, weakness):
+    keys = [k for k, v in weakness.items() if t in v]
+    return keys
 
 
-check_weakness(my_types)
+def get_type_effectiveness(t, weakness):
+    weak_against = check_weakness(t, weakness)
+    strong_against = check_strength(t, weakness)
+
+    print(f"Your Pokemon is strong against {strong_against}\nYour Pokemon is weak against {weak_against}")
+
+get_type_effectiveness('Water', weakness_matrix)
